151b8fa350e5ce7a1cb6f55385da5f374d7461ca
EshaAmjad18 <eshamjad6868@gmail.com>
2026-03-31 19:24:28 +0500

    fixed Violation model

---
+++ backend/src/models/Violation.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const violationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  order: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order'
+  },
+  type: {
+    type: String,
+    enum: ['order_not_picked', 'repeated_violation'],
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Violation', violationSchema);
